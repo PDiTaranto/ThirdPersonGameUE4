eb46d561ad57e775fac328d70053dbf3423c5cda
Pablo Di Taranto <pablo.ditaranto.dev@gmail.com>
2023-01-25 15:54:14 -0300

    Project Creation

---
+++ Source/Final/Public/Character/FinalPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "FinalPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FINAL_API AFinalPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
